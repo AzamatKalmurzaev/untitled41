b875a04a7a2bf6f13985e0b924eabc33ae5bdd2f
ASUS <Azamatkal87@gmail.com>
2022-09-07 19:53:14 +0300

    birinchi Comit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Student student = new Student("Timur", 20);
+        System.out.println(student);
+
+
     }
+
+
+
 }
